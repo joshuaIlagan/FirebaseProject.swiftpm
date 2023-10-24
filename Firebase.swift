938605cb3c2be632612b91205cba64d8f568e7d5
Joshua Ilagan <Jilagan6194@stu.d214.org>
2023-10-24 10:36:02 -0500

    firebase stuff

---
+++ Firebase.swift
@@ -0,0 +1,32 @@
+//
+//  Firebase.swift
+//  FirebaseProject
+//
+//  Created by Joshua S. Ilagan on 10/24/23.
+//
+
+import Foundation
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
+
+@main
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+      }
+    }
+  }
+}
