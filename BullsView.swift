0d4406386b0d7a6125a77425e0921816f736fe77
Andres Lopez <alopez4578@stu.d214.org>
2023-11-01 11:17:38 -0500

    ibis

--- BullsView.swift
+++ BullsView.swift
@@ -9,25 +9,48 @@ import SwiftUI
 import FirebaseDatabase
 
 struct BullsView: View {
-    
-    
     let database = Database.database().reference()
-    @State var teamName = ""
-    @State var PlayerName = ""
-    @State var PlayerNumber = ""
-    
-    
+    @State private var teamName = ""
+    @State private var playerName = ""
+    @State private var playerNumber = ""
     
     var body: some View {
-        VStack{
-        TextField("Team", text: $teamName)
-        TextField("Player", text: $PlayerName)
-        TextField("Number", text: $PlayerNumber)
+        Color.red
+                .ignoresSafeArea()
+                .blendMode(.multiply)// Ignore just for the color
+                .overlay(
+        VStack {
+            Image("bulls")
+                .offset(y:-20)
+                
+            
+            TextField("Team", text: $teamName)
+                .padding()
+                .border(Color.black, width: 4)
+                .background(.white)
+            
+            TextField("Player", text: $playerName)
+                .padding()
+                .border(Color.black, width: 4)
+                .background(.white)
+            
+            TextField("Number", text: $playerNumber )
+                .padding()
+                .border(Color.black, width: 4)
+                .background(.white)
             Button("Add Entry") {
-                var dict = [PlayerName : PlayerNumber]
-                database.child(teamName).setValue(dict)
+                var dict = [playerName: playerNumber]
+                database.child(teamName).childByAutoId().setValue(dict)
             }
-        }
+            .padding()
+            .background(Color.black)
+            .foregroundColor(.white)
+            .cornerRadius(20)
+            Image("goat")
+                .resizable()
+                .frame(width: 200, height: 250)
+        })
+        
     }
 }
 
--- LakersView.swift
+++ LakersView.swift
@@ -19,15 +19,36 @@ struct LakersView: View {
     
     
     var body: some View {
+        Color.yellow
+                .ignoresSafeArea()
+                .blendMode(.multiply)// Ignore just for the color
+                .overlay(
         VStack{
+            Image("lakers")
+                .offset(y:-50)
+            
         TextField("Team", text: $teamName)
+                .padding()
+                .border(Color.black, width: 4)
+                .background(.white)
         TextField("Player", text: $PlayerName)
+                .padding()
+                .border(Color.black, width: 4)
+                .background(.white)
         TextField("Number", text: $PlayerNumber)
+                .padding()
+                .border(Color.black, width: 4)
+                .background(.white)
             Button("Add Entry") {
                 var dict = [PlayerName : PlayerNumber]
                 database.child(teamName).childByAutoId().setValue(dict)
             }
-        }
+            .padding()
+            .background(Color.purple)
+            .foregroundColor(.white)
+            .cornerRadius(20)
+            Image("LEBRON")
+        })
     }
 }
 
